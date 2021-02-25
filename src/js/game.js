9841188b95945cc6a42272bad05de9d0375400ae
ItsmeLee <arsmagica90@gmail.com>
2021-02-25 22:06:42 +0900

    Add timer and score functions.
    Add timer and score functions in game.js file.
    When user correct the quiz the score plus 1 and update the quiz.
    When game user click the game start button, the game and timer start immediately.

--- src/js/game.js
+++ src/js/game.js
@@ -3,6 +3,12 @@ const gameStartBtn = document.querySelector("button");
 const displayQuiz = document.querySelector(".quiz");
 const answerInput = document.querySelector("input");
 const answerForm = document.querySelector("form");
+const timer = document.querySelector(".left__time");
+
+let countTimer;
+let count = 0;
+let time = 60;
+
 
 // 문제 출제 함수
 function askQuiz(){
@@ -21,21 +27,49 @@ function userAnswer(event){
     answerInput.value = "";
 }
 
+// 점수 부여 함수
+function giveScore(){
+    const point = document.querySelector(".collect__quiz");
+    point.innerHTML = `${count + 1}`;
+    count = count + 1;
+}
+
 // 채점 함수
 function checkCorrect(quiz, answer){
     if(quiz === answer){
-        console.log('Correct');
         askQuiz();
+        giveScore();
     }
     else{
-        console.log('Wrong!');
+        console.log(`Wrong!~`);
     }
+}
+
+// 타이머 종료 함수
+function clearTimer(){
+    if(time < 0){
+        time = 60;
+        clearInterval(countTimer);
+    }
+}
+
+// 제한 시간 관리 함수
+function countTime(){
+    timer.innerHTML = `${time}`;
+    time--;
+    clearTimer();
+}
+
+// 타이머 시작 함수
+function startTimer(){
+    countTimer = setInterval(countTime, 1000);
 }
 
 // 게임 시작함수
 function gameStart(){
     askQuiz();
     answerForm.addEventListener("submit", userAnswer);
+    startTimer();
 }
 
 // 게임에 관련된 모든 기능들을 관리하는 함수
