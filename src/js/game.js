0b99e67c5a48b4eb3f947e5946c62a7957cf1810
ItsmeLee <arsmagica90@gmail.com>
2021-02-25 16:15:14 +0900

    Check function.
    Add quiz correct function in game.js file.
    If the user typing corrected answer in input box, display "correct!" in console.
    If not correct the answer, the console display "Worng!".

--- src/js/game.js
+++ src/js/game.js
@@ -1,6 +1,8 @@
 import { words } from "./words.js";
 const gameStartBtn = document.querySelector("button");
 const displayQuiz = document.querySelector(".quiz");
+const answerInput = document.querySelector("input");
+const answerForm = document.querySelector("form");
 
 // 문제 출제 함수
 function askQuiz(){
@@ -10,9 +12,30 @@ function askQuiz(){
 }
 
 
+// 사용자 답 확인 함수
+function userAnswer(event){
+    event.preventDefault();
+    const currentAnswer = answerInput.value;
+    const currentQuizText = displayQuiz.textContent;
+    checkCorrect(currentQuizText, currentAnswer);
+    answerInput.value = "";
+}
+
+// 채점 함수
+function checkCorrect(quiz, answer){
+    if(quiz === answer){
+        console.log('Correct');
+        askQuiz();
+    }
+    else{
+        console.log('Wrong!');
+    }
+}
+
 // 게임 시작함수
 function gameStart(){
     askQuiz();
+    answerForm.addEventListener("submit", userAnswer);
 }
 
 // 게임에 관련된 모든 기능들을 관리하는 함수
