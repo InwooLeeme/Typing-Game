cc78e6f913a6cdfa653fd224bd13ed506497a04d
ItsmeLee <arsmagica90@gmail.com>
2021-02-26 14:22:08 +0900

    Add game stop function.
    Add game stop function in game.js file.
    Add game stop function in game.js file.
    If the timer will be 0, the game stop, display game result page and the point will be reset.

--- src/js/game.js
+++ src/js/game.js
@@ -1,13 +1,19 @@
 import { words } from "./words.js";
+
+const gameContainer = document.querySelector(".game__container");
 const gameStartBtn = document.querySelector("button");
 const displayQuiz = document.querySelector(".quiz");
 const answerInput = document.querySelector("input");
 const answerForm = document.querySelector("form");
 const timer = document.querySelector(".left__time");
+const point = document.querySelector(".collect__quiz");
+const gameResult = document.querySelector(".game__result__container");
 
 let countTimer;
+const GAMETIME = 60;
 let count = 0;
-let time = 60;
+let time = GAMETIME;
+let isPlaying = false;
 
 
 // 문제 출제 함수
@@ -29,7 +35,6 @@ function userAnswer(event){
 
 // 점수 부여 함수
 function giveScore(){
-    const point = document.querySelector(".collect__quiz");
     point.innerHTML = `${count + 1}`;
     count = count + 1;
 }
@@ -45,11 +50,47 @@ function checkCorrect(quiz, answer){
     }
 }
 
+// 
+
+function redirectHome(){
+    const gameResultPage = document.querySelector(".game__result");
+    gameContainer.classList.remove("hide");
+    gameResult.removeChild(gameResultPage);
+    gameResult.style.height = 0;
+}
+
+// 게임 종료 함수
+function stopGame(){
+    isPlaying = false;
+    clearTimer();
+    const div = document.createElement("div");
+    const h1 = document.createElement("h1");
+    const h2 = document.createElement("h2");
+    const btn = document.createElement("button");
+    h1.innerHTML = `Game Result`;
+    h2.innerHTML = `점수 : ${count}`;
+    btn.innerHTML = "재시작";
+    btn.addEventListener("click", redirectHome);
+    div.classList.add("game__result");
+    gameResult.appendChild(div);
+    div.appendChild(h1);
+    div.appendChild(h2);
+    div.appendChild(btn);
+    gameContainer.classList.add("hide");
+    gameResult.style.height = "80%";
+    gameResult.classList.remove("hide");
+    point.innerHTML = `0`;
+    displayQuiz.innerHTML = `Quiz`;
+    count = 0;
+    gameStartBtn.innerHTML = `게임 시작`;
+}
+
 // 타이머 종료 함수
 function clearTimer(){
     if(time < 0){
-        time = 60;
+        time = GAMETIME;
         clearInterval(countTimer);
+        stopGame();
     }
 }
 
@@ -67,9 +108,14 @@ function startTimer(){
 
 // 게임 시작함수
 function gameStart(){
+    if(isPlaying){
+        return;
+    }
+    isPlaying = true;
     askQuiz();
     answerForm.addEventListener("submit", userAnswer);
     startTimer();
+    gameStartBtn.innerHTML = `게임중`;
 }
 
 // 게임에 관련된 모든 기능들을 관리하는 함수
